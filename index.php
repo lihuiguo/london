b2b34454e5642dcea6a6cb2b76e0610f951b0761
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-11-30 01:05:09 -0500

    began adding content/structure
    created class "login-link" for use when designing in CSS.; made link to login page. and link to new user page.

--- index.php
+++ index.php
@@ -3,7 +3,41 @@
 <head>
 	<title>Visiting London</title>
 </head>
-<body class="z-patttern">
 
-</body>
+<header class="persistent">
+			<div class="container">
+				<h1 class="lead">Visit London!</h1>
+			<section class="strong-follow">
+				
+				<p>
+					<a href="info.php"> 
+						See what London has to offer!
+					</a>
+				</p>
+			</section>
+			</div>
+		</header>
+		
+		<main>
+			<div id="big-message" class="hero"> 
+				<h2>Featuring top US climbers!</h2>
+			</div>
+		</main>
+
+		<footer class="persistent">
+			<div class="container">
+				<section class="weak-follow">
+				<a href="login.php" class="login-link">
+					Admin Login
+				</a>
+					<form> 
+						Email <input type="text" name="email">
+						<input type="submit" name="submit">
+					</form>
+				</section>
+				<h2 class="terminal">
+					<a href="profiles.php" class="non-icon-link">Climber Profiles →</a> 
+				</h2>
+			</div>
+		</footer>
 </html>
