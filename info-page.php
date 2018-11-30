e5340ccc4e8ee61fabc869bbcf2e6a3b685e17e1
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-11-30 01:10:44 -0500

    began laying groundwork skeleton for info page

---
+++ info-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>London Info</title>
+</head>
+<body>
+	<h1>
+		London Calling!
+	</h1>
+
+	<h2>
+		Food & Drink
+	</h2>
+		<p>
+			
+		</p>
+
+	<h2>
+		History
+	</h2>
+		<p>
+			
+		</p>
+
+	<h2>
+		Attractions
+	</h2>
+		<p>
+			
+		</p>
+</body>
+</html>
