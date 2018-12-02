252b8814e4d678abc95dd03acd4307083817ac21
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-12-02 18:15:38 -0500

    addedd lorem ipsum to skeleton

--- info-page.php
+++ info-page.php
@@ -9,14 +9,14 @@
 		London Calling!
 	</h1>
 
-	<main>
+	<main class="container">
 		
 
 	<h2>
 		Food & Drink
 	</h2>
 		<p>
-			
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		</p>
 		<img src=" ">
 
@@ -24,7 +24,7 @@
 		History
 	</h2>
 		<p>
-			
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		</p>
 		<img src="">
 
@@ -32,7 +32,7 @@
 		Attractions
 	</h2>
 		<p>
-			
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		</p>
 		<img src="">
 
