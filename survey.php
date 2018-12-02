aa1e1d8ffa373c518278c15af1940ad389c55fd3
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-12-02 18:15:46 -0500

    began creating form

--- survey.php
+++ survey.php
@@ -4,6 +4,28 @@
 	<title>London-Survey</title>
 </head>
 <body>
-	<!-- Radio Buttons/Forms here-->
+	<main>
+		<form>
+			<p>
+				First Name <input type="text" name="firstname">
+			</p>
+
+			<p>
+				Last Name <input type="text" name="lastname">
+			</p>
+			<p>
+				Have you ever visited London before? 
+				<input type="radio" name="have-visited" value="yes"> Yes
+				<input type="radio" name="have-visited" value="no"> No
+			</p>
+			<p>
+				If so, when? 
+				<input type="text" name="date-visited">
+			</p>
+			<
+			<!-- Radio Buttons/Forms here-->
+		</form>
+	</main>
+
 </body>
 </html>
