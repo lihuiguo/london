0c96b9738db36397c9ac485cefbf743121131248
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-12-02 23:50:24 -0500

    create thanks.php
    made appropriate landing page, with a link back to the index

---
+++ thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thanks!</title>
+</head>
+<body>
+	<div>
+		Thank you for your input! 
+	</div>
+	<div>
+		<a href="index.php"> Home</a>
+	</div>
+</body>
+</html>
