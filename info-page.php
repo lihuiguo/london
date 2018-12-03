90e9cfa1c46e91f358320b8a12808fcd97db9b00
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-12-02 23:33:33 -0500

    updated info page

--- info-page.php
+++ info-page.php
@@ -16,7 +16,7 @@
 		Food & Drink
 	</h2>
 		<p>
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			Looking for the best restaurants in London? Our complete guide points you in the right direction. Are you craving something specific? How about the best burgers in London? Or, why not tuck into a juicy steak? If you want to try something authentic, try the best fish and chips London has to offer; or indulge in the best afternoon teas in London, a real British tradition. For special occasions, discover some of London’s best romantic or quirky restaurants, as well as all the Michelin-starred restaurants London has to offer. Quench your thirst at one of the many London cafes or visit some of the best London rooftop bars. Celebrate with a drink at a cocktail bar or champagne bar in London before heading out and discovering more things to do in London.
 		</p>
 		<img src="img/afternoon-tea.jpg" alt="pastries and fine china decorate this afternoon tea table">
 
@@ -39,7 +39,12 @@
 	</main>
 </body>
 <footer>
-	<a href="index.php" >Home</a>
-	<a href="survey.php" >Take our survey!</a>
+	<div>
+		<a href="index.php" >Home</a>
+	</div>
+	<div>
+		<a href="survey.php" >Take our survey!</a>
+	</div>
+	
 </footer>
 </html>
