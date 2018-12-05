84721caa90269289de829d74b2e6548c7a662d81
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-12-05 16:27:39 -0500

    updated survey

--- survey.php
+++ survey.php
@@ -23,13 +23,23 @@
 				Last Name <input type="text" name="last_name">
 			</div>
 			<div>
-				Have you ever visited London before?
-				<input type="radio" name="has_visited" value="yes"> Yes
-				<input type="radio" name="has_visited" value="no"> No
+				What aspect of the City of London most interests you?
+				<input type="radio" name="has_visited" value="food-and-drink"> Food and Drink
+				<input type="radio" name="has_visited" value="history"> History
+				<input type="radio" name="has_visited" value="cultural-attractions"> Cultural Attractions
+				<input type="radio" name="has_visited" value="all"> All of the above
 			</div>
 			<div>
-				If so, when?
-				<input type="text" name="date_visited">
+				What type of food and drink are you interested in learning more about? 
+				<input type="checkbox" name="food" value="Afternoon-Tea"> Afternoon Tea
+				<input type="checkbox" name="food" value="Pubs-and-Bars"> Pubs and Bars 
+				<input type="checkbox" name="food" value="Fine-Dining"> Fine Dining 
+			</div>
+			<div>
+				What type of cultural attractionsare you most interested in? 
+				<input type="checkbox" name="attractions" value="parks-and-gardens"> Parks and Gardens
+				<input type="checkbox" name="attractions" value="amusements"> Family Amusements 
+				<input type="checkbox" name="attractions" value="museums-and-galleries"> Museums and Galleries 
 			</div>
 			<div>
 				Would you like to visit London?
